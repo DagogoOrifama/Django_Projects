502a130383a3dfd3adfc266d4b49f81426037b09
Dagogo Orifama <dagoris2010@gmail.com>
2024-08-17 21:27:07 +0100

    creating food app db_interaction

---
+++ mysite/food/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+# model are blueprints to create database tables, they define
+# the structure of how the models will be created (eg it defines
+# the table columns, names, datatypes, maximum values it can contain, etc)
+class Item(models.Model):
+    # defining the string representation of the models
+    def __str__(self):
+        return self.item_name
+    
+    item_name = models.CharField(max_length=200)
+    item_desc = models.CharField(max_length=200)
+    item_price = models.IntegerField()
+ 
